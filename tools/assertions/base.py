a39c5cb661ef6862be48fbdb6dddd60bd962ee63
alagamba <the.life.is.on@gmail.com>
2026-09-17 11:56:01 +0300

    login-autotest

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,5 +1,7 @@
 from typing import Any
 
+from clients.authentication.authentication_schema import LoginResponseSchema
+
 
 def assert_status_code(actual: int, expected: int):
     """
@@ -30,3 +32,23 @@ def assert_eq(actual: Any, expected: Any, name: str):
         f'Expected value: "{expected}". '
         f'Actual value: "{actual}".'
     )
+
+
+def assert_is_true(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение является истинным.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение ложно.
+    """
+    assert actual, (
+        f'Incorrect value: "{name}". '
+        f'Expected true value but got: {actual}'
+    )
+
+
+def assert_login_response(response: LoginResponseSchema):
+    assert_eq(response.token.token_type, "bearer", "token_type")
+    assert_is_true(response.token.access_token, "access_token")
+    assert_is_true(response.token.refresh_token, "refresh_token")
